a618f1421043fb3600b382a3615e0bd6e53f9e13
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-11-03 23:46:50 +0600

    variable

---
+++ variables.cs
@@ -0,0 +1,17 @@
+using System;
+
+// variable
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jubayer";
+            Console.WriteLine(myName);
+        }
+    }
+}
+
+// end code
